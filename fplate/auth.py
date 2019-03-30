906e7117dcbde5cf6accb44dd8a35e7e91d9729c
Oleksandr Zinchenko <zinchenko@live.com>
2019-03-30 10:46:12 -0700

    crud for stock and fixed auth

--- fplate/auth.py
+++ fplate/auth.py
@@ -8,66 +8,60 @@ from fplate.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', methods=('GET', 'POST'))
+@bp.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        req_json = request.get_json()
-        username = req_json['username']
-        password = req_json['password']
+    req_json = request.get_json()
+    username = req_json['username']
+    password = req_json['password']
 
-        db = get_db()
-        error = None
+    db = get_db()
+    error = None
 
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
-            error = 'User {} is already registered.'.format(username)
+    if not username:
+        error = 'Username is required.'
+    elif not password:
+        error = 'Password is required.'
+    elif db.execute(
+        'SELECT id FROM user WHERE username = ?', (username,)
+    ).fetchone() is not None:
+        error = 'User {} is already registered.'.format(username)
 
-        if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return make_response('', 200)
+    if error is None:
+        db.execute(
+            'INSERT INTO user (username, password) VALUES (?, ?)',
+            (username, generate_password_hash(password))
+        )
+        db.commit()
+        return make_response('Created', 201)
 
-        flash(error)
-
-    return make_response('err', 401)
+    return make_response(error, 404)
 
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        req_json = request.get_json()
-        username = req_json['username']
-        password = req_json['password']
+    req_json = request.get_json()
+    username = req_json['username']
+    password = req_json['password']
 
-        db = get_db()
-        error = None
+    db = get_db()
+    error = None
 
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+    user = db.execute(
+        'SELECT * FROM user WHERE username = ?', (username,)
+    ).fetchone()
 
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
+    if user is None:
+        error = 'Incorrect username.'
+    elif not check_password_hash(user['password'], password):
+        error = 'Incorrect password.'
 
-        if error is None:
-            # JWT
-            session.clear()
-            session['user_id'] = user['id']
-            return make_response('OK', 200)
+    if error is None:
+        # JWT
+        session.clear()
+        session['user_id'] = user['id']
+        return make_response('OK', 200)
 
-        flash(error)
-
-    return make_response('NG', 401)
+    return make_response(error, 404)
 
 
 @bp.route('/logout')
@@ -92,7 +86,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return make_response('failed', 400)
+            return make_response('unauthorized', 401)
 
         return view(**kwargs)
 
--- fplate/stock.py
+++ fplate/stock.py
@@ -1,10 +1,70 @@
-from flask import (Blueprint, jsonify, flash, g, redirect, render_template, request, url_for)
+from flask import (Blueprint, jsonify, flash, g, request, make_response)
 from werkzeug.exceptions import abort
 
 from fplate.auth import login_required
 from fplate.db import get_db
 
-bp = Blueprint('stock', __name__)
+bp = Blueprint('stock', __name__, url_prefix='/stock')
+
+
+@bp.route('/create', methods=['POST'])
+@login_required
+def create():
+    req_json = request.get_json()
+    symbol = req_json['symbol']
+    company = req_json['company']
+    total_count = req_json['total_count']
+    price = req_json['price']
+
+    if get_stock(symbol) is not None:
+        return make_response('already exist', 409)
+
+    db = get_db()
+    db.execute(
+        'INSERT INTO stock (symbol, company, total_count, price) '
+        'VALUES (?, ?, ?, ?)', (symbol, company, total_count, price))
+    db.commit()
+
+    return make_response('Created', 201)
+
+
+@bp.route('/<string:symbol>/update', method=['PATCH'])
+@login_required
+def update(symbol):
+    rj = request.get_json()
+    company = rj['company']
+    total_count = rj['total_count']
+    price = rj['price']
+
+    if get_stock(symbol) is None:
+        return make_response('Does not exist', 404)
+
+    db = get_db()
+    db.execute(
+        'UPDATE stock '
+        'SET company = ?, total_count = ?, price = ? '
+        'WHERE symbol = ?',
+        (company, total_count, price, symbol))
+    db.commit()
+
+    return make_response('Updated', 200)
+
+
+@bp.route('/<string:symbol>/delete', methods=['DELETE'])
+@login_required
+def delete(symbol):
+    get_stock(symbol)
+    db = get_db()
+    db.execute('DELETE * FROM stock WHERE symbol = ?', [symbol])
+    db.commit()
+    return make_response('OK', 200)
+
+
+@bp.route('/<string:symbol>', methods=['GET'])
+def get(symbol):
+    stock = get_stock(symbol)
+    return jsonify(stock)
+
 
 @bp.route('/', methods=['GET'])
 def get_all():
@@ -13,12 +73,6 @@ def get_all():
              'FROM stock')
     stocks = db.execute(query).fetchall()
     return jsonify(stocks)
-
-
-@bp.route('/<string:symbol>', methods=['GET'])
-def get(symbol):
-    stock = get_stock(symbol)
-    return jsonify(stock)
 
 
 def get_stock(symbol):
@@ -32,4 +86,3 @@ def get_stock(symbol):
         abort(404, "Stock {stock} doesn't exist".format(stock=stock))
 
     return stock
-
